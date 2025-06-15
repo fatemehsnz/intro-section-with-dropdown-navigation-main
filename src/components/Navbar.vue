4f5cc445e4cffea4545f704e1eb91d30b395a5da
fatemeh <fatemeh.hasanzadeh@yahoo.com>
2025-06-15 19:23:26 +0330

    change navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,40 +1,138 @@
 <template>
-    <div class="flex justify-between py-6 px-8">
-        <div class="flex gap-10 ">
-            <img src="../assets/images/logo.svg" alt="Logo" />
-            <ul class="flex gap-8">
-                <li class="relative" @mouseenter="show = true" @mouseleave="show=false">
-                    <button class="flex gap-2">
-                    Features
-                    <img src="../assets/images/icon-arrow-down.svg" alt="Dropdown" class="w-2 h-1 mt-3" :class="{ 'rotate-180': show }" />
-                    </button>
+  <div class="flex justify-between py-6 px-8">
+    <div class="flex items-center gap-24">
+      <img src="../assets/images/logo.svg" alt="Logo" />
+      <ul class="flex gap-8">
+        <li
+          class="relative"
+          @mouseenter="show = true"
+          @mouseleave="show = false"
+        >
+          <button
+            class="flex gap-2 font-epilogue font-medium text-mediumgray hover:text-black"
+          >
+            Features
+            <img
+              src="../assets/images/icon-arrow-down.svg"
+              alt="Dropdown"
+              class="w-2 h-1 mt-2.5"
+              :class="{ 'rotate-180': show }"
+            />
+          </button>
 
-                    <div v-if="show" class="absolute top-full py-2 px-4 bg-white ">
-                        <a href="#" >Todo List</a>
-                    </div>
-                </li>
+          <div
+            v-if="show"
+            class="absolute top-full bg-white shadow-xl rounded-md w-36 right-0 py-4 px-4"
+          >
+            <a
+              href="#"
+              class="flex items-center gap-3 font-epilogue font-medium text-mediumgray hover:text-black"
+            >
+              <img src="../assets/images/icon-todo.svg" alt="Todo Icon" />
+              <span>Todo List</span>
+            </a>
+            <a
+              href="#"
+              class="flex items-center gap-3 py-1 font-epilogue font-medium text-mediumgray hover:text-black"
+            >
+              <img
+                src="../assets/images/icon-calendar.svg"
+                alt="Calendar Icon"
+                class="w-4 h-4"
+              />
+              <span>Calendar</span>
+            </a>
+            <a
+              href="#"
+              class="flex items-center gap-3 py-1 font-epilogue font-medium text-mediumgray hover:text-black"
+            >
+              <img
+                src="../assets/images/icon-reminders.svg"
+                alt="Reminders Icon"
+                class="w-4 h-4"
+              />
+              <span>Reminders</span>
+            </a>
+            <a
+              href="#"
+              class="flex items-center gap-3 py-1 font-epilogue font-medium text-mediumgray hover:text-black"
+            >
+              <img
+                src="../assets/images/icon-planning.svg"
+                alt="Planning Icon"
+                class="w-4 h-4"
+              />
+              <span>Planning</span>
+            </a>
+          </div>
+        </li>
+        <li
+          class="relative"
+          @mouseenter="showcompany = true"
+          @mouseleave="showcompany = false"
+        >
+          <button
+            class="flex gap-2 font-epilogue font-medium text-mediumgray hover:text-black"
+          >
+            Company
+            <img
+              src="../assets/images/icon-arrow-down.svg"
+              alt="Dropdown"
+              class="w-2 h-1 mt-2.5"
+              :class="{ 'rotate-180': showcompany }"
+            />
+          </button>
 
-                <li class="flex gap-2">
-                    Company
-                    <img src="../assets/images/icon-arrow-down.svg" alt="Dropdown" class="w-2 h-1 mt-3" />
-                </li>
-                <li>Careers</li>
-                <li>About</li>
-            </ul>
-        </div>
-        <div class="flex gap-10">
-            <button class="py-1 px-3 rounded-xl border-2 border-transparent hover:border-black">
-                Login
-            </button>
-            <button class="py-1 px-3 rounded-xl border-2 border-transparent hover:border-black">
-                Register
-            </button>
-        </div>
+          <div
+            v-if="showcompany"
+            class="absolute top-full bg-white shadow-xl rounded-md w-28 left-0 py-4 px-4"
+          >
+            <a
+              href="#"
+              class="flex items-center gap-3 font-epilogue font-medium text-mediumgray hover:text-black"
+            >
+              <span>History</span>
+            </a>
+            <a
+              href="#"
+              class="flex items-center gap-3 py-1 font-epilogue font-medium text-mediumgray hover:text-black"
+            >
+              <span>Our Team</span>
+            </a>
+            <a
+              href="#"
+              class="flex items-center gap-3 py-1 font-epilogue font-medium text-mediumgray hover:text-black"
+            >
+              <span>Blog</span>
+            </a>
+          </div>
+        </li>
+        <li class="font-epilogue font-medium text-mediumgray hover:text-black">
+          <a href="#"> Careers </a>
+        </li>
+        <li class="font-epilogue font-medium text-mediumgray hover:text-black">
+          <a href="#"> About </a>
+        </li>
+      </ul>
     </div>
+    <div class="flex items-center gap-10">
+      <button
+        class="py-1 px-3 rounded-xl border-2 border-transparent hover:border-black font-epilogue font-medium"
+      >
+        Login
+      </button>
+      <button
+        class="py-1 px-3 rounded-xl border-2 border-transparent hover:border-black font-epilogue font-medium"
+      >
+        Register
+      </button>
+    </div>
+  </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref } from "vue";
 
-const show = ref(false)
+const show = ref(false);
+const showcompany = ref(false);
 </script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        'almostwhite': 'hsl(0, 0%, 98%)',
-        'mediumgray' : 'hsl(0, 0%, 41%)'
+        almostwhite: "hsl(0, 0%, 98%)",
+        mediumgray: "hsl(0, 0%, 41%)",
       },
       fontFamily: {
-        vazir: ['Vazir', 'sans-serif'],
-      }
+        epilogue: ["Epilogue", "sans-serif"],
+      },
     },
   },
   plugins: [],
-}
-
+};
